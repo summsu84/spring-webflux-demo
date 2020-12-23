fc816c3506f6ef65fe0770fbbbf4666a0d0ce7ea
summsu84 <summsu84@gmail.com>
2020-12-23 21:35:00 +0900

    1. webflux 데모 추가

---
+++ spring-webflux-demo/src/main/java/com/teamjw/SpringWebfluxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.teamjw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxDemoApplication.class, args);
+	}
+
+}
