c68755ec4cf88068a5d5eb957e2fd1a6660461a6
summsu84 <summsu84@gmail.com>
2021-02-20 17:25:01 +0900

    mod

--- spring-webflux-demo/src/main/java/com/teamjw/SpringWebfluxDemoApplication.java
+++ spring-webflux-demo/src/main/java/com/teamjw/SpringWebfluxDemoApplication.java
@@ -2,10 +2,27 @@ package com.teamjw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class SpringWebfluxDemoApplication {
 
+	
+	WebClient client = WebClient.create();
+	
+	static final String URL1 = "http://localhost:8082/service?req={req}";
+	static final String URL2 = "http://localhost:8082/service2?req={req}";
+	
+	@GetMapping("/rest")
+	public Mono<String> rest(int idx) {
+		// Mono<ClientResponse>
+		return null;
+		
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebfluxDemoApplication.class, args);
 	}
